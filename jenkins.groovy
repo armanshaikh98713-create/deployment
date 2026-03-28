6d551a017d54edfe88db79438cf2cc6502e68616
shkarman12 <shaikharman79000@gmail.com>
2026-03-28 18:09:38 +0530

    new changes

--- jenkins.groovy
+++ jenkins.groovy
@@ -15,7 +15,7 @@ pipeline {
         }
     
     }
-    stage('sending code'){
+    stage('deplyment'){
             sshagent(['nginx']){
                 sh "scp -o StrictKeyChecking=no *ubuntu@13.234.35.251:tmp" 
             }
