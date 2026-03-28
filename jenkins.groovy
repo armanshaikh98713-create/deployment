0acfd9b4c6b757e0b3b848ae50b9430a133504fa
shkarman12 <shaikharman79000@gmail.com>
2026-03-28 17:33:04 +0530

    code chnages

--- jenkins.groovy
+++ jenkins.groovy
@@ -4,14 +4,15 @@ pipeline {
     stages {
         stage('Clone Repo') {
             steps {
-                git branch: 'main', url: 'https://github.com/armanshaikh98713-create/university-website.git'
+                git branch: 'main', url: 'https://github.com/armanshaikh98713-create/arman.git'
             }
         }
 
         stage('Build') {
             steps {
                 echo 'Build Stage'
-            }
+    
         }
     }
+}
 }
