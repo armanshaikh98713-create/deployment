ec50f51fa8b1406b6a617d02dd02aedc69e5bc61
shkarman12 <shaikharman79000@gmail.com>
2026-03-28 17:48:34 +0530

    changes

--- jenkins.groovy
+++ jenkins.groovy
@@ -13,6 +13,23 @@ pipeline {
                 echo 'Build Stage'
     
         }
+    pipeline {
+    agent any
+
+    stages {
+        stage('Clean Workspace') {
+            steps {
+                deleteDir()   // 🔥 deletes everything
+            }
+        }
+
+        stage('Clone Repo') {
+            steps {
+                git branch: 'main', url: 'https://github.com/armanshaikh98713-create/university-website.git'
+            }
+        }
+    }
+}
     }
 }
 }
